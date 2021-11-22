18482bf3cba39e581d8b0233e53ad937781ae2cf
Aysel ipek <aysel.ipek@bil.omu.edu.tr>
2021-11-22 22:41:25 +0300

    edit theme

--- MvcCompanyCall/Controllers/DefaultController.cs
+++ MvcCompanyCall/Controllers/DefaultController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace MvcCompanyCall.Controllers
 {
-    public class LayoutController : Controller
+    public class DefaultController : Controller
     {
         // GET: Default
         public ActionResult Index()
