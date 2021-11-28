dc36d42ed6ac445123a87a11cb7c370f8ea754da
Aysel ipek <aysel.ipek@bil.omu.edu.tr>
2021-11-28 15:56:51 +0300

    add authorize attribute

--- MvcCompanyCall/Controllers/DefaultController.cs
+++ MvcCompanyCall/Controllers/DefaultController.cs
@@ -7,20 +7,25 @@ using MvcCompanyCall.Models.Entity;
 
 namespace MvcCompanyCall.Controllers
 {
+    [Authorize]
     public class DefaultController : Controller
     {
         // GET: Default
+        
         public ActionResult Index()
         {
             return View();
         }
 
         DbJobTrackingEntities db = new DbJobTrackingEntities();
+        
         public ActionResult ActiveCalls()
         {
             var calls = db.TblCall.Where(x =>x.CallStatus == true && x.CallCompany == 4).ToList();
             return View(calls);
         }
+
+
         public ActionResult PassiveCalls()
         {
             var calls = db.TblCall.Where(x => x.CallStatus == false && x.CallCompany == 4).ToList();
